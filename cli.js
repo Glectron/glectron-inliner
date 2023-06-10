ed9bdf85bef56c6f3f4477c397d6919a983919bd
YUCLing <luotianyi@luotianyi.me>
2023-06-10 17:51:13 +0800

    add: watch mode

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 
+import watch from "node-watch";
 import minimist from 'minimist';
 import inline from './index.js';
 
@@ -8,10 +9,29 @@ const argv = minimist(process.argv.slice(2));
 
 const targetFile = argv._[0];
 const outputFile = argv.o || argv.output;
+const watchDir = argv.w || argv.watch;
 
-const result = await inline(path.resolve(process.cwd(), targetFile));
-if (outputFile) {
-    fs.writeFileSync(path.resolve(process.cwd(), outputFile), result, "utf-8");
-} else {
-    console.log(result);
+async function runInline() {
+    const result = await inline(path.resolve(process.cwd(), targetFile));
+    if (outputFile) {
+        fs.writeFileSync(path.resolve(process.cwd(), outputFile), result, "utf-8");
+    } else {
+        console.log(result);
+    }
+}
+
+await runInline();
+
+if (watchDir) {
+    console.log("Watching", path.resolve(process.cwd(), watchDir), "for changes...");
+    const watcher = watch(path.resolve(process.cwd(), watchDir), {recursive: true}, async (_e, _file) => {
+        await runInline();
+        console.log("File updated");
+    });
+    process.on('SIGINT', () => {
+        watcher.close();
+    });
+    process.on('SIGTERM', () => {
+        watcher.close();
+    });
 }
